3d6dafca4647f03388b321e64cdd1d352009dc0f
vinicius <vinicius853o@gmail.com>
2022-08-15 18:38:10 -0300

    fix

--- lib/app/pages/homepage/widgets/image_card_widget.dart
+++ lib/app/pages/homepage/widgets/image_card_widget.dart
@@ -1,5 +1,7 @@
 import 'package:aval3_nasa/app/data/models/image_planetary_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:image_network/image_network.dart';
 
 class CardTile extends StatelessWidget {
   ImageUniverse infosPlanet;
@@ -11,29 +13,10 @@ class CardTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        width: 100,
-        height: 200,
-        child: Image.network(
-          infosPlanet.url,
-          fit: BoxFit.fill,
-          scale: 1.0,
-          errorBuilder: (context, exception, stackTrace) {
-            return Text('Erro ao carregar a imagem😥');
-          },
-          loadingBuilder: (BuildContext context, Widget child,
-              ImageChunkEvent? loadingProgress) {
-            if (loadingProgress == null) return child;
-            return Center(
-              child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                    : null,
-              ),
-            );
-          },
-        ),
+      child: CachedNetworkImage(
+        imageUrl: infosPlanet.url,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
       ),
     );
   }
